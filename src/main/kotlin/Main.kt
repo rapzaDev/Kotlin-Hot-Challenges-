fa5237e079cb2b76480e8146e6d1662f5749c325
rapzaDev <rapzadev@gmail.com>
2022-11-06 22:23:51 -0300

    Challenge 2 completed

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 import java.util.*
 
 fun main(args: Array<String>) {
-    challenge1()
+    println(challenge2("Challenge 2 completed"))
 }
 
 /*
@@ -16,17 +16,41 @@ fun challenge1() {
         index -> mapOf(
             0 to index,
                 index % 3 to "Fizz",
-                index % 5 to "Buzz"
+                index % 5 to "Buzz",
+                index % 15 to "Fizz Buzz"
         )[0]
     })
 }
 
 /*
-* Explanation: Map used for each index value ( 1 to 100 )
+* Explanation:
+* Map used for each index value ( 1 to 100 )
 * the println function prints always '0' to $index. But, bellow this condition,
 * I have: index % 3 to "Fizz". So, if index is some multiple of 3 the conditional value is 0.
 * In this logic, if (0 to index) && (index % 3 == 0) then 0 to "Fizz". Because, in the first line,
 * 0 to index is always true, but in the next line, 0 is also true for "Fizz", so 0 is now the KET for
 * the value of the condition.
 * And to put all of this information on an array, in the end of $mapOf(), I'm only want the values of the KEY '0'.
+* */
+
+
+
+/*
+* 2)
+* Given an input string , reverse the string word by word
+* Input: "reverse words in a string"
+* Output: "string a in words reverse"
+* */
+
+fun challenge2(stringToReverse: String) =
+    stringToReverse
+        .split(" ")
+        .reversed()
+        .joinToString(" ")
+
+/*
+* Explanation:
+* Apply the split(" ") function to eliminate all the blank spaces on th string.
+* Use the reversed() function in order to return a list with elements in reversed order.
+* And finally, use the joinToString(" ") function to add a space blank between all position of the string.
 * */
